c6989898dea766a694c8d2b7eb86c5d7b92d83f1
Samuel David Rojas Vallejo <samueldavidrojasvallejo@gmail.com>
2025-05-27 11:18:35 -0500

    feat: Crear tipos por defecto

---
+++ migrations/0006_crear_tipos_iniciales.down.sql
@@ -0,0 +1,8 @@
+DELETE FROM
+  tipos
+WHERE
+  id IN (
+    '0197174e-fd6a-746c-9fdf-690f48a3c9a1',
+    '0197174f-2132-7411-bd69-19c7bea4eef5',
+    '0197174f-3d03-777f-9aa9-4a7cb55f009d'
+  );
---
+++ migrations/0006_crear_tipos_iniciales.up.sql
@@ -0,0 +1,24 @@
+INSERT INTO
+  tipos(id, nombre, color)
+VALUES
+  (
+    '0197174e-fd6a-746c-9fdf-690f48a3c9a1',
+    'Película',
+    '#D93526'
+  );
+INSERT INTO
+  tipos(id, nombre, color)
+VALUES
+  (
+    '0197174f-2132-7411-bd69-19c7bea4eef5',
+    'Serie',
+    '#CD68E0'
+  );
+INSERT INTO
+  tipos(id, nombre, color)
+VALUES
+  (
+    '0197174f-3d03-777f-9aa9-4a7cb55f009d',
+    'Libro',
+    '#029AE8'
+  );
